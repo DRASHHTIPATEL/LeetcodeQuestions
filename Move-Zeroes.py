7b59b4b8084491d73e037d478947d07e6cea4f4b
Drashti Patel <90262225+DRASHHTIPATEL@users.noreply.github.com>
2025-11-04 23:52:43 -0500

    [283] [Time Beats: 51.49%] [Memory Beats: 9.77%] - LeetPush

---
+++ Move-Zeroes.py
@@ -0,0 +1,13 @@
+
+        
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        n = len(nums)
+        i = 0  # pointer for the position of next non-zero
+
+        for j in range(n):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+
+
