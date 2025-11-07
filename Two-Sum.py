532d88d5104a6fb044ae7295420431fc5d3f6df0
Drashti Patel <90262225+DRASHHTIPATEL@users.noreply.github.com>
2025-11-06 22:39:49 -0500

    [1] [Time Beats: 100.00%] [Memory Beats: 51.29%] - LeetPush

--- Two-Sum.py
+++ Two-Sum.py
@@ -1,13 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-# we created a dict to store nums seen so far from the array
-        preMap={}#val: index
-# we find diff for eacy and then check if its in our hashmap .. return index of it and index of the current
-        for i,n in enumerate(nums):
-            diff=target-n
-            if diff in preMap:
-                return[preMap[diff],i]
-            preMap[n]=i
-            
-        return
-        
+        n=len(nums)
+        hash={}
+        for i in range(0,n):
+            remaining=target-nums[i]
+            if remaining in hash:
+                return [hash[remaining],i]
+            hash[nums[i]]=i
